a7c8d6dbd258ca95239ea94697887f7f6eca0544
ririnandayani <ririn.andayani77@gmail.com>
2024-06-20 20:47:32 +0700

    Update alksaslk.py

--- alksaslk.py
+++ alksaslk.py
@@ -1,10 +1,1 @@
-import numpy as np
-from matplotlib.image import imread
-import matplotlib.pyplot as plt
 
-image_raw = imread("/REVOU/walpeper.jpeg")
-print(image_raw.shape)
-
-# Displaying the image
-plt.figure(figsize=[12,8])
-plt.imshow(image_raw)
